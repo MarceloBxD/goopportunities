163346ff57f3c04ae690dece7616a2625930261d
Marcelo <marcelo.bracet1@gmail.com>
2025-01-04 20:45:43 -0300

    feat: add a config package

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/MarceloBxD/goopportunities/config"
 	"github.com/MarceloBxD/goopportunities/router"
 )
 
 func main() {
+	err := config.Init()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// initalize router
 	router.Initialize()
 }
